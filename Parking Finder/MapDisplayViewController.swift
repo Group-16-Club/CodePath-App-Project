50b39a4b10b0514fbb0fc989ce2fedb114a68f65
trishtruong <trishtruong@utexas.edu>
2022-10-29 23:38:46 -0500

    added MapView and display current user location

--- Parking Finder/MapDisplayViewController.swift
+++ Parking Finder/MapDisplayViewController.swift
@@ -6,15 +6,39 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapDisplayViewController: UIViewController {
+class MapDisplayViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet var mapView: MKMapView!
+    var locationManager : CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.distanceFilter = 200
+        locationManager.requestWhenInUseAuthorization()
     }
-    
+
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == CLAuthorizationStatus.authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: false)
+        }
+    }
 
     /*
     // MARK: - Navigation
