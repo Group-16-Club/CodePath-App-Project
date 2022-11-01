de956dabebafed37a39125f11d656a74bfd9513e
CyberHotpot <xinninghuang2020@outlook.com>
2022-11-01 16:41:22 -0400

    Add API call

--- Parking Finder/MapDisplayViewController.swift
+++ Parking Finder/MapDisplayViewController.swift
@@ -14,16 +14,43 @@ class MapDisplayViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet var mapView: MKMapView!
     var locationManager : CLLocationManager!
+    var longitude:Double!
+    var latitude:Double!
+    var token:String!
+    var parkingLots:[[String:Any]]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
         locationManager.distanceFilter = 200
         locationManager.requestWhenInUseAuthorization()
+        
+        // Get current longitude and latitude
+        var currentLoc: CLLocation!
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse || CLLocationManager.authorizationStatus() == .authorizedAlways) {
+                 currentLoc = locationManager.location
+                 latitude=currentLoc.coordinate.latitude
+                 longitude=currentLoc.coordinate.longitude
+        }
+        /*
+        // Make API call
+        let url = URL(string: "https://api.parkme.com/lots?accesstoken=\(token)&point=\(latitude)|\(longitude)&radius=75&locale=en-US")!
+
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                self.parkingLots=dataDictionary["results"] as! [[String:Any]]
+            }
+        }
+        task.resume()
+    */
     }
 
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
